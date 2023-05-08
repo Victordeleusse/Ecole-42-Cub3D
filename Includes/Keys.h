f84e0b5d842b8b3b690cc7a7532eeff4921c67d1
Victor DE LEUSSE <vde-leus@e2r12p18.clusters.42paris.fr>
2023-05-08 20:44:06 +0200

    Avancement

---
+++ Includes/Keys.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Keys.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-leus <vde-leus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 20:41:00 by vde-leus          #+#    #+#             */
+/*   Updated: 2023/05/08 20:43:57 by vde-leus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEYS_H
+# define KEYS_H
+
+// # define MOUSE_LEFT			
+// # define MOUSE_RIGHT		
+# define KEY_ESC			65307
+
+# define KEY_LEFT			65361
+# define KEY_RIGHT			65363
+# define KEY_UP				65362
+# define KEY_DOWN			65364
+
+#endif
+
